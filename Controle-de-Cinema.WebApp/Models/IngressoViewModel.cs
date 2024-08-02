7dfbeb4d3fb39c5c7b7dbbb2e12b69a13a02599c
Luiz Gustavo Hemkemaier Santos <lghs60@hotmail.com>
2024-08-02 16:05:55 -0300

    Iniciando Modulo (ViewModel Ingresso pronta)

---
+++ Controle-de-Cinema.WebApp/Models/IngressoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Controle_de_Cinema.Dominio;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Controle_de_Cinema.WebApp.Models;
+
+public class IngressoViewModel
+{
+    public int Id { get; set; }
+    public bool MeiaEntrada { get; set; }
+    public Pessoa Pessoa { get; set; }
+    public Sessao Sessao { get; set; }
+}
+
+
